d274b3415dc3fa85b96f9f9d12a05954c10b06af
mein9 <mateo0221@gmail.com>
2024-05-19 12:36:49 -0700

    added new lines after each print

--- game.py
+++ game.py
@@ -9,16 +9,16 @@ i = 1
 tries = 6
 while i in range(6):
     if c != d:
-        print(f'{c} was not correct. You have {tries} more tries.')
+        print(f'{c} was not correct. You have {tries} more tries.\n')
         tries -= 1
         if c > d:
-            print("The number was lower ")
+            print("The number was lower \n")
         if c < d:
-            print("The number was higher ")
-        c = int(input("Please Guess Again "))
+            print("The number was higher \n")
+        c = int(input("Please Guess Again \n "))
     
         if c == d:
-            print(f'Congratulations you guessed the number {d} correctly')
+            print(f'Congratulations you guessed the number {d} correctly \n')
 
 else:
-    print("Sorry you were not able to correctly guess the number in 7 tries. Please try again.")
+    print("Sorry you were not able to correctly guess the number in 7 tries. Please try again. /n")
