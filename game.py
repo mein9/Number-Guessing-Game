3729e9f1fab9f74039522302c9df473a2351e854
mein9 <mateo0221@gmail.com>
2024-05-19 13:45:03 -0700

    corrected variables to be more readable

--- game.py
+++ game.py
@@ -1,25 +1,30 @@
 import random
 import math
 
-a = int(input("Enter Lower Limit "))
-b = int(input("Enter Upper Limit "))
-c = int(input(f'Guess a number between {a} and {b} '))
-d = random.randrange(a,(b + 1))
+lower_limit = int(input("Enter Lower Limit "))
+upper_limit = int(input("Enter Upper Limit "))
+user_input = int(input(f'Guess a number between {lower_limit} and {upper_limit} '))
+rand_num = random.randrange(lower_limit,(upper_limit + 1))
 i = 0
-tries = 7
-while i < 7:
-    if c != d:
-        print(f'{c} was not correct. You have {tries} more tries.', end=" ")
-        tries -= 1
-        i += 1
-        if c > d:
+while i < math.log(upper_limit - user_input +1, 2):
+    i += 1
+    if user_input != rand_num:
+        print(f'{user_input} was not correct.', end=" ")
+        
+        if user_input > rand_num:
             print("The number was lower ")
-        if c < d:
+        if user_input < rand_num:
             print("The number was higher ")
-        c = int(input("Please Guess Again \n "))
-    
-        if c == d:
-            print(f'Congratulations you guessed the number {d} correctly ')
 
-else:
-    print("Sorry you were not able to correctly guess the number in 7 tries. Please try again. \n ")
+        user_input = int(input("Please Guess Again \n "))
+    
+        if user_input == rand_num:
+            print(f'Congratulations you guessed the number {d} correctly in {i} tries ')
+
+if i >= math.log(upper_limit - user_input + 1, 2):
+    print(f'Sorry you were not able to correctly guess the number in {i} tries. The correct answer was {d} \n ')
+   #play_again = input("Play again? (y/n)")
+    #if (play_again == "y") or (play_again == "Y") :
+        #find out how to start code over
+   # if (play_again == n) or (play_again == "N"):
+       # print("Goodbye")
