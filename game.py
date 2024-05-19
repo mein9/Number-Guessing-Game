5acb066b7fecdcb2127f380a7266480dcb69f002
mein9 <mateo0221@gmail.com>
2024-05-19 15:58:45 -0700

    used chat gpt to help me figure out how to have the game replay based on user in put

--- game.py
+++ game.py
@@ -1,28 +1,36 @@
 import random
 import math
 
-lower_limit = int(input("Enter Lower Limit "))
-upper_limit = int(input("Enter Upper Limit "))
+def main_program():
+    lower_limit = int(input("Enter Lower Limit "))
+    upper_limit = int(input("Enter Upper Limit "))
 
-max_tries = int(math.log(upper_limit - lower_limit + 1, 2))
-rand_num = random.randrange(lower_limit,(upper_limit + 1))
-i = 0
-while i < max_tries:
-    user_input = int(input(f'Guess a number between {lower_limit} and {upper_limit}: '))
-    i += 1
-    if user_input == rand_num:
-        print(f'Congratulations! You guessed the number {rand_num} correctly in {i} tries.')
-        break
-    else:
-        print(f'{user_input} was not correct.', end=" ")
-        if user_input > rand_num:
-            print("The number is lower.")
+    max_tries = int(math.log(upper_limit - lower_limit + 1, 2))
+    rand_num = random.randrange(lower_limit,(upper_limit + 1))
+    i = 0
+    while i < max_tries:
+        user_input = int(input(f'Guess a number between {lower_limit} and {upper_limit}: '))
+        i += 1
+        if user_input == rand_num:
+            print(f'Congratulations! You guessed the number {rand_num} correctly in {i} tries.')
+            break
         else:
-            print("The number is higher.")
-        
-
-else:
-    print(f'Sorry you were not able to correctly guess the number in {i} tries. The correct answer was {rand_num} \n ')
+            print(f'{user_input} was not correct.', end=" ")
+            if user_input > rand_num:
+                print("The number is lower.")
+            else:
+                print("The number is higher.")
+    else:
+        print(f'Sorry you were not able to correctly guess the number in {i} tries. The correct answer was {rand_num} \n ')
+def main():
+    while True:
+        main_program()
+        play_again = input("Play again? (y/n)").strip().lower()
+        if play_again != 'y':
+            print("Goodbye!")
+            break
+if __name__ == "__main__":
+    main()
    #FIXME figure out how to make this outline work lol
    #play_again = input("Play again? (y/n)")
     #if (play_again == "y") or (play_again == "Y") :
