3c341f8533b325caf4da8362d15fb31176990695
mein9 <mateo0221@gmail.com>
2024-05-19 12:51:36 -0700

    changed the loop

--- game.py
+++ game.py
@@ -5,20 +5,21 @@ a = int(input("Enter Lower Limit "))
 b = int(input("Enter Upper Limit "))
 c = int(input(f'Guess a number between {a} and {b} '))
 d = random.randrange(a,(b + 1))
-i = 1
-tries = 6
-while i in range(6):
+i = 0
+tries = 7
+while i < 7:
     if c != d:
-        print(f'{c} was not correct. You have {tries} more tries.\n')
+        print(f'{c} was not correct. You have {tries} more tries.', end=" ")
         tries -= 1
+        i += 1
         if c > d:
-            print("The number was lower \n")
+            print("The number was lower ")
         if c < d:
-            print("The number was higher \n")
+            print("The number was higher ")
         c = int(input("Please Guess Again \n "))
     
         if c == d:
-            print(f'Congratulations you guessed the number {d} correctly \n')
+            print(f'Congratulations you guessed the number {d} correctly ')
 
 else:
-    print("Sorry you were not able to correctly guess the number in 7 tries. Please try again. /n")
+    print("Sorry you were not able to correctly guess the number in 7 tries. Please try again. \n ")
